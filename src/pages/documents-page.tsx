1552a221bf02be1594f3dc65c0fad6721dae586a
Mateus Landim <mateus@MacBook-Pro-de-Mateus.local>
2026-06-10 17:37:36 -0300

    fix: mesh added to mobile side bar and documents page

--- src/pages/documents-page.tsx
+++ src/pages/documents-page.tsx
@@ -1,9 +1,11 @@
 import Container from "../components/container";
+import ChatBackGroundMesh from "../core-components/chat-background-mesh";
 import DocumentsList from "../core-components/documents-list";
 
 export default function DocumentsPage() {
   return(
     <Container as="article" className="space-y-3 py-3">
+      <ChatBackGroundMesh />
       <DocumentsList />
     </Container>
   )
